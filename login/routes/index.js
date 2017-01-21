88dd878f88cb56ead5012de0395d8a24225bbccb
Tom <tomshaddock@googlemail.com>
2017-01-21 15:21:54 +0000

    reorder the pages and implement some customised messages

--- login/routes/index.js
+++ login/routes/index.js
@@ -2,9 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 //Load the home page
-router.get('/', function(req, res){
+router.get('/', ensureAuthenticated, function(req, res){
 	//render the index view
 	res.render('index');
 });
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		req.flash('error_msg', 'Sorry, you have not logged in.');
+		res.redirect('/users/login');
+	}
+}
 
 module.exports = router;
